fdb0c48a3ce4be36442f3b20641a2ee9d570bf3f
mehresyd <93683641+mehresyd@users.noreply.github.com>
2023-01-25 14:09:53 -0600

    Create unit4Functions.php

---
+++ unit4Functions.php
@@ -0,0 +1,65 @@
+<?php 
+
+$timestamp = date("U");
+$string = " My school is DMACC. ";
+$phoneNumber = 1234567890;
+$usDollars = 123456;
+
+
+
+function getMDYFormat($timestamp) {
+return date("m/d/Y", $timestamp);
+    
+}
+
+function getDMYFormat($timestamp) {
+return date("d/m/Y", $timestamp);
+}
+
+function displayStringInput($string) {
+ //Display number of characters in the string 
+ echo "Number of characters: " . strlen($string) . "<br>";
+
+ //Trim any leading or trailing whitespace
+ $string = trim($string);
+ echo "After trimming whitespace: " . $string . "<br>";
+
+ //Display the string as all lowercase characters 
+ $string = strtolower($string);
+ echo "After converting to lowercase: " . $string . "<br>";
+
+
+ if (stripos($string, "DMACC") !== false) {
+    echo "String contains 'DMACC'";
+ } else {
+    echo "String does not contain 'DMACC'";
+ }
+
+}
+
+function formattedNumber($phoneNumber) {
+    return "(" . substr($phoneNumber, 0, 3) . ") " . substr($phoneNumber, 3, 3) . "-" . substr($phoneNumber, 6);
+}
+
+function formattedCurrency($usDollars) {
+   return "$" . number_format($usDollars, 2); 
+} 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Unit 4-1: PHP Functions</title>
+</head>
+<body>
+    <h1>4-1: PHP Functions</h1>
+    <p>mm/dd/yyyy format: <?php echo getMDYFormat($timestamp);?></p>
+    <p>dd/mm/yyyy format: <?php echo getDMYFormat($timestamp);?></p>
+    <?php echo displayStringInput($string);?>
+    <p>Number displayed as formatted phone number: <?php echo formattedNumber($phoneNumber);?></p>
+    <p>Number displayed as formatted US currency: <?php echo formattedCurrency($usDollars);?></p>
+</body>
+</html>
